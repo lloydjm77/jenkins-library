7064e2e8700495cd1f50ac58610b62a9220fa457
Jonathan Lloyd <lloydjm77@gmail.com>
2017-04-04 20:50:50 -0500

    Create hello.groovy

---
+++ hello.groovy
@@ -0,0 +1,12 @@
+class hello implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
